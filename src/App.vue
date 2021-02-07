c986ef374c90cd2b311a4dcbd021aa6f66e2746b
rinao <180233999@qq.com>
2021-02-07 10:09:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Park3D />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Park3D from "./components/Park3D.vue";
+import "./utils/fixGltf";
+
+@Options({
+  components: {
+    Park3D
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+html {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/Park3D.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="park3d" id="park3d" />
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import Renderer from "./renderer";
+
+export default class Park3d extends Vue {
+  renderer: Renderer;
+
+  constructor(props: unknown) {
+    super(props);
+    this.renderer = new Renderer();
+  }
+
+  mounted() {
+    this.init();
+  }
+
+  init = (): void => {
+    this.renderer.init();
+  }
+}
+</script>
+
+<style scoped>
+.park3d {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
